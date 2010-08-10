c11d9aa8fc6be61af2ee56bdb76ff44f98de9711
sarnowski <sarnowski@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-08-10 12:58:06 +0000

    initial version of palava-ipc-cache-php

---
+++ src/main/java/de/cosmocode/palava/ipc/cache/php/CachedWeaver.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.cosmocode.palava.ipc.cache.php;
+
+import com.google.inject.Inject;
+import de.cosmocode.commons.reflect.Reflection;
+import de.cosmocode.palava.core.Registry;
+import de.cosmocode.palava.ipc.cache.Cached;
+import de.cosmocode.palava.ipc.json.custom.CustomPostCallEvent;
+import de.cosmocode.palava.ipc.json.custom.CustomProtocol;
+import de.cosmocode.palava.ipc.protocol.DetachedConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * Adds a flag to the response, that the command may be cached
+ *
+ * @author Tobias Sarnowski
+ */
+final class CachedWeaver implements CustomPostCallEvent {
+    private static final Logger LOG = LoggerFactory.getLogger(CachedWeaver.class);
+
+    // for php
+    private static final String CACHED_KEY = "CACHED";
+
+    @Inject
+    CachedWeaver(Registry registry) {
+        registry.register(CustomPostCallEvent.class, this);
+    }
+
+    @Override
+    public void eventPostCall(Map<String, Object> request, Map<String, Object> response, DetachedConnection connection) {
+        // get the command class manually
+        final String cmd = String.class.cast(request.get(CustomProtocol.COMMAND));
+        final Class command;
+        try {
+            command = Reflection.forName(cmd);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("cannot retrieve the command " + cmd + " as class", e);
+        }
+
+        // weave the "CACHED" constant into the meta informations
+        response.put(CACHED_KEY, command.isAnnotationPresent(Cached.class));
+        LOG.trace("Weaved response with {}: {}", CACHED_KEY, response.get(CACHED_KEY));
+    }
+}
---
+++ src/main/resources/palava/modules/IpcCache.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * Caches command results and immidiatly returns them
+ * if remote command is @Cached
+ */
+class IpcCache extends AbstractPalavaModule {
+
+    const NAME = __CLASS__;
+
+    const PKEY_CACHED = "CACHED";
+
+    const CONFIG_ENABLED = "cache.enabled";
+    const CONFIG_COMMANDS = "cache.commands";
+    const DEFAULT_ENABLED = true;
+
+    public static $DEFAULT_COMMANDS = array("de.cosmocode.palava.util.ValuesOf");
+
+    private $cache = array();
+
+    private $oneShot = false;
+
+    private function enabled() {
+        return $this->get(IpcCache::CONFIG_ENABLED, IpcCache::DEFAULT_ENABLED);
+    }
+
+    private function callKey(&$call) {
+        $key = array(
+            $call[Palava::PKEY_SESSION],
+            $call[Palava::PKEY_COMMAND],
+            $call[Palava::PKEY_ARGUMENTS]
+        );
+        return json_encode($call);
+    }
+
+    public function oneShot() {
+        $this->oneShot = true;
+    }
+
+    private function isConfigCached(&$call) {
+        $commands = $this->get(IpcCache::CONFIG_COMMANDS, IpcCache::$DEFAULT_COMMANDS);
+        $command = $call[Palava::PKEY_COMMAND];
+
+        if (in_array($command, $commands)) {
+            return $command;
+        } else {
+            return null;
+        }
+    }
+
+    public function preCall(&$call) {
+        // module disabled?
+        if (!$this->enabled()) {
+            return null;
+        }
+
+        // cache disabled for one call?
+        if ($this->oneShot) {
+            $this->oneShot = false;
+            return null;
+        }
+
+        // cached by config?
+        if (($key = $this->isConfigCached($call)) === null) {
+            $key = $this->callKey($call);
+        }
+
+        // return the cached result if available        
+        if (isset($this->cache[$key])) {
+            return $this->cache[$key];
+        } else {
+            return null;
+        }
+    }
+
+    public function postCall(&$call, &$result) {
+        // module disabled?
+        if (!$this->enabled()) {
+            return;
+        }
+
+        if (isset($result[Palava::PKEY_EXCEPTION])) {
+            // do not cache exceptions
+            return;
+        }
+
+        if (($key = $this->isConfigCached($call)) === null) {
+            // get meta informations
+            if (!isset($result[IpcCache::PKEY_CACHED])) {
+                throw new Exception(IpcCache::PKEY_CACHED." not set; PhpCacheModule installed?");
+            }
+
+            if ($result[IpcCache::PKEY_CACHED]) {
+                $key = $this->callKey($call);
+            }
+        }
+
+        // if meta CACHED is true, cache it
+        if (!is_null($key)) {
+            $this->cache[$key] = $result;
+        }
+    }
+
+}
