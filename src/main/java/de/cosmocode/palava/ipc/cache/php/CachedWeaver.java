ba0494813676fbeb85825052ef15f3ec40672c65
Oliver Lorenz <lorenz@cosmocode.de>
2012-02-24 23:10:35 +0100

    Revert "increased parent version, made ipc 2.0 compliant"
    This reverts commit d94f1df0e0566dd237e9e733890d4b46785f9764.
    
    This is done because a test build of two production environments using palava broke.
    The next step will be to release a bugfix version, then the commit will be re-reverted later and a major version will be released.

--- src/main/java/de/cosmocode/palava/ipc/cache/php/CachedWeaver.java
+++ src/main/java/de/cosmocode/palava/ipc/cache/php/CachedWeaver.java
@@ -16,21 +16,17 @@
 
 package de.cosmocode.palava.ipc.cache.php;
 
-import java.lang.annotation.Annotation;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.inject.Inject;
-
 import de.cosmocode.commons.reflect.Reflection;
 import de.cosmocode.palava.core.Registry;
-import de.cosmocode.palava.ipc.IpcCommand;
-import de.cosmocode.palava.ipc.cache.ComplexCacheAnnotation;
+import de.cosmocode.palava.ipc.cache.Cached;
 import de.cosmocode.palava.ipc.json.custom.CustomPostCallEvent;
 import de.cosmocode.palava.ipc.json.custom.CustomProtocol;
 import de.cosmocode.palava.ipc.protocol.DetachedConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 /**
  * Adds a flag to the response, that the command may be cached.
@@ -54,26 +50,17 @@ final class CachedWeaver implements CustomPostCallEvent {
         DetachedConnection connection) {
         // get the command class manually
         final String cmd = String.class.cast(request.get(CustomProtocol.COMMAND));
-        final Class<? extends IpcCommand> command;
+        final Class<?> command;
         
         try {
-            command = Reflection.forName(cmd).asSubclass(IpcCommand.class);
+            command = Reflection.forName(cmd);
         } catch (ClassNotFoundException e) {
             throw new IllegalStateException("cannot retrieve the command " + cmd + " as class", e);
         }
 
         // weave the "CACHED" constant into the meta informations
-        response.put(CACHED_KEY, isCacheable(command));
+        response.put(CACHED_KEY, command.isAnnotationPresent(Cached.class));
         LOG.trace("Weaved response with {}: {}", CACHED_KEY, response.get(CACHED_KEY));
-    }
-    
-    private boolean isCacheable(Class<? extends IpcCommand> command) {
-        for (Annotation annotation : command.getAnnotations()) {
-            if (annotation.annotationType().isAnnotationPresent(ComplexCacheAnnotation.class)) {
-                return true;
-            }
-        }
-        return false;
     }
     
 }
